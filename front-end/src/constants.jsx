cd211e534dd24a99627a41d6de6aaffef4bb0fc0
TheDenGaming <azurecoder@DESKTOP-R904J4A.localdomain>
2022-09-23 00:37:31 -0400

    Second Commit

---
+++ front-end/src/constants.jsx
@@ -0,0 +1,12 @@
+const CONTRACT_ADDRESS = '0xe3268f406CA58b2868c09a33a61A394F4c23f59e';
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
